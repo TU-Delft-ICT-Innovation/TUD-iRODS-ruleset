e90a23791ee6a8988c432c227f39ab4bd45ec67e
Marcel Heijink <m.j.heijink@tudelft.nl>
2018-02-21 16:15:02 +0100

    rule to remove objects not used a long time

---
+++ tudDeleteNotAccessedObjects.r
@@ -0,0 +1,18 @@
+tudDeleteNotAccessedObjects {                                                                                                  
+    *Q1 = select COLL_NAME, DATA_NAME, DATA_SIZE, META_DATA_ATTR_VALUE where META_DATA_ATTR_NAME = 'LAST_ACCESS_TIME';	
+    msiGetSystemTime(*Time, "unix");
+
+    foreach (*R1 in *Q1) {
+        *elapsed = *Time - double(*R1.META_DATA_ATTR_VALUE);
+        *objPath = *R1.COLL_NAME ++ "/" ++ *R1.DATA_NAME;
+
+        if (*elapsed > 10000) {
+	    writeLine("stdout", "De volgende is te oud, gooi weg! *objPath");
+	    msiDataObjUnlink("objPath=*objPath++++forceFlag=", *Status);
+	} else {
+            writeLine("stdout", "De volgende is niet zo oud, bewaren! *objPath  *elapsed");
+        }
+    }	
+}
+INPUT null
+OUTPUT ruleExecOut
